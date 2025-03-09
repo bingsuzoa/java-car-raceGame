57a7e85ed8a0f1baf6b0cdc2d1f813ba69717064
bingsuzoa <zxc_777@naver.com>
2025-03-09 18:37:40 +0900

    test : 유효한 입력 확인하는 테스트 추가

--- src/main/java/com/gameRace/controller/InputValidator.java
+++ src/main/java/com/gameRace/controller/InputValidator.java
@@ -5,14 +5,14 @@ import com.gameRace.view.Message;
 public class InputValidator {
 
     public boolean validateCarNames(String input) {
-        if (input.matches("^.*,.+$")) {
+        if (input.matches("^[^\\W_].*,[^,\\W_]$")) {
             return true;
         }
         throw new IllegalArgumentException(Message.ERROR_INVALID_INPUT.getMessage());
     }
 
     public boolean validateTryNumber(String input) {
-        if (input.matches("^[1-9]+$")) {
+        if (input.matches("^[1-9]\\d*$")) {
             return true;
         }
         throw new IllegalArgumentException(Message.ERROR_INVALID_INPUT.getMessage());
